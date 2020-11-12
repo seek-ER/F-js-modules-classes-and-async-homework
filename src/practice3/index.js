9eac2deee0a7a023ecc158eebb43e3901bf5e455
Anson <1072476988@qq.com>
2020-11-12 12:18:14 +0800

    [Lingzhen] feat: finish practice3.

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,18 @@
-export const getPoetry
+// export const getPoetry = () =>{
+//     return fetch("https://v1.jinrishici.com/all.json")
+//     .then(
+//         response => response.json()
+//     ).then(
+//         ({content, origin, author}) => [origin, author, content]
+//     ).catch(error => console.log(error))
+// }
+
+export const getPoetry = async() => {
+    try {
+        const response = await fetch("https://v1.jinrishici.com/all.json")
+        const {content, origin, author} = await response.json()
+        return [origin, author, content]
+    } catch (error) {
+        console.log(error)
+    }
+}
